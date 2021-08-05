3f921afef0d7c4a80f0d7fa318581481b0bcd69a
Navjyotsinghoberai <52479490+Navjyotsinghoberai@users.noreply.github.com>
2021-08-05 12:55:36 +0530

    version 3

--- main.js
+++ main.js
@@ -24,28 +24,34 @@
 
 // xhr.send()
 
-const textBox=document.getElementById('text-input')
+const textBox=document.getElementById('input-text')
+// const textBox=document.getElementById('text-input')
 const button=document.getElementById('search-btn')
 
-console.log(button);
-button.addEventListener('click',()=>{
+// console.log(button);
+button.addEventListener('click',(e)=>{
     const movieName = textBox.value
     const xhr = new XMLHttpRequest();
     const url = `https://api.themoviedb.org/3/search/movie?api_key=abdca3eea1b7fb0c1f10423e2fc33135&query=${movieName}`
     xhr.open('GET',url)
     // check
-    console.log('success');
-    xhr.onreadystatechange=() => {
-        console.log("hi");
+    // console.log('success');
+    e.preventDefault()
+    xhr.onreadystatechange = () => {
         if(xhr.readyState==4 && xhr.status==200){
             const movieData=JSON.parse(xhr.response)
-            const movieUrl = document.createElement('video')
+            console.log(movieData); //just to check the api parameters
+            const movieImage = document.createElement('img')
+            
             // const movieUrl=document.createElement('movie-url')
-            movieUrl.setAttribute('src', movieData.url)
-            console.log(movieUrl);
+            movieImage.innerHTML = movieData.results[0].backdrop_path
+            // movieUrl.setAttribute('src', movieData.url)
+            console.log(movieImage);
             const container=document.getElementById('image-container')
+            container.appendChild(movieImage)
+            
+            
         }
     }
     xhr.send()
-
 })
